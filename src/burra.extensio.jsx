635e4b4aeebc33004ae68e125812841ef61a6034
Ruben <1511ruben@gmail.com>
2024-10-05 00:56:57 -0600

    aaa

---
+++ src/burra.extensio.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function burra() {
+  return (
+    <div>
+      <ul>
+        <li>a</li>
+        <li>tg</li>
+        <li>d</li>
+        <li>f</li>
+        <li>g</li>
+      </ul>
+    </div>
+  )
+}
